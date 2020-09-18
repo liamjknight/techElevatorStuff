3b44b76a7f709dfa75e63e25ec3fe4e3c7ee5d96
Liam Knight <liamjknight@gmail.com>
2020-09-18 18:30:14 -0400

    Saving Day 5 in case I mess something up

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,41 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class Fibonacci {
+	
+	public void fibPrint(int highEnd) {
+		int[] fibStart = {0, 1};
+		String fibString = "";
+		
+		if (highEnd<1) {
+			System.out.println(fibStart[0]);
+		}else if (highEnd==1) {
+			System.out.println(fibStart[0]+", "+fibStart[1]);
+		}else {
+			int holdCurrentNum;
+			int currentNum = 1;
+			Integer previousNum = 0;
+			do {
+				fibString = fibString + previousNum.toString() + ", ";
+				holdCurrentNum = currentNum;
+				currentNum += previousNum;
+				previousNum = holdCurrentNum;
+			} while (previousNum<=highEnd);
+		}
+		System.out.println(fibString);
+	}
 
 	public static void main(String[] args) {
-
+		Fibonacci fibbing = new Fibonacci();
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Please enter the Fibonacci number: ");
+		
+		String highEndString = userInput.nextLine();
+		int highEndInt = Integer.parseInt(highEndString);
+		
+		fibbing.fibPrint(highEndInt);
+		
+	
 	}
 
 }
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,27 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
+	public void conversionMethod(int length, String type) {
+		if (type=="m") {
+			int toFeet = (int)(length * 3.2808399);
+			System.out.println(toFeet);
+		}else if (type=="f") {
+			int toMeters = (int)(length * 0.3048);
+			System.out.println(toMeters);
+		}
+	}
 
 	public static void main(String[] args) {
-
+		Scanner userInput = new Scanner(System.in);
+		LinearConvert convert = new LinearConvert();
+		
+		System.out.println("Please enter the length: ");
+		int length = Integer.parseInt(userInput.nextLine());
+		System.out.println("Is the measurement in (m)eter, or (f)eet?");
+		String type = userInput.nextLine();
+		convert.conversionMethod(length, type);
 	}
 
 }
