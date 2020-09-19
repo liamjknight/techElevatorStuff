4c18be29fbc6f8bcbfa0e764734e4830ccc92821
Liam Knight <liamjknight@gmail.com>
2020-09-19 16:32:25 -0400

    Day 5 Homework complete

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class LinearConvert {
 
 	public void meterFeetConversion(double length, String type) {
-		if (type=="m") {//toFeet
+		if (type.equals("m")) {//toFeet
 			double toFeet = (length * 3.2808399);
 			int toFeetFinal = (int)toFeet;
 			System.out.println(toFeetFinal);
-		}else if (type=="f") {//toMeters
+		}else if (type.equals("f")) {//toMeters
 			double toMeters = (length * 0.3048);
 			int toMetersFinal = (int)toMeters;
 			System.out.println(toMetersFinal);
