47f6a443c6727b9b419984ca1ad2a3030133efcb
Liam Knight <liamjknight@gmail.com>
2020-10-05 17:51:50 -0400

    day 16 homework

--- module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
@@ -1,10 +1,67 @@
 package com.techelevator;
 
-public class QuizMaker {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class QuizMaker {
+	static Scanner userInput = new Scanner(System.in);
+
+	public static void main(String[] args) throws FileNotFoundException {
 		
+		File testFile = getFile();
+		int totalQuestions = 0;
+		int totalAnsweredCorrectly = 0;
+		
+		try(Scanner testScanner = new Scanner(testFile)){
+			
+			while(testScanner.hasNextLine()) {
+				//takes each line and creates question
+				totalQuestions++;
+				String inputString = testScanner.nextLine();
+				String inputSplit[] = inputString.split("[|]");//[|] needed or else the split happens each char
+				int correctAnswer=0;
+				for(int i = 0; i < inputSplit.length; i++) {
+		
+					if(inputSplit[i].contains("*")) {
+						correctAnswer = i;
+						inputSplit[i] = inputSplit[i].replace("*", "");
+					}
+					
+					if(i>0)	{
+						System.out.println(i+") "+inputSplit[i]);
+					}else {
+						System.out.println(inputSplit[i]);
+					}
+				}//request answer
+				System.out.println("Please enter the *number* that corrosponds to your answer");
+				int answer = Integer.parseInt(userInput.nextLine());
+				if(answer == correctAnswer) {
+					totalAnsweredCorrectly++;
+					System.out.println("Your answer "+answer+" is correct!");
+				}else {
+					System.out.println("Your answer "+answer+" is incorrect.");
+				}
+			}
+			testScanner.close();
+			System.out.printf("You got %d answer(s) correct out of the total %d questions asked.", totalAnsweredCorrectly, totalQuestions);
+		}
 
 	}
-
+	
+	
+	private static File getFile() {
+		System.out.println("Enter the quiz file");
+		File testFile = new File(userInput.nextLine());
+		if(!testFile.exists()) {
+			System.out.println("File does not exist");
+			System.exit(1);
+		}
+		if(!testFile.isFile()) {
+			System.out.println("Entred URI is not a file");
+			System.exit(1);
+		}
+		return testFile;
+	}
 }
--- module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,10 +1,64 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class WordSearch {
-
-	public static void main(String[] args) {
 	
-
+	public static void main(String[] args) {
+		Scanner consoleIn = new Scanner(System.in);
+		File fileToSearch;
+		
+		//checking if user inputed file exists
+		System.out.println("What is the file that should be searched?");
+		String file = consoleIn.nextLine();
+		fileToSearch = new File(file);
+		if(!fileToSearch.exists()) {
+			System.out.println("File does not exist");
+			System.exit(1);
+		}
+		if(!fileToSearch.isFile()) {
+			System.out.println("Entred URI is not a file");
+			System.exit(1);
+		}
+		
+		//getting search phrase
+		System.out.println("What is the search word you are looking for?");
+		String searchPhrase = consoleIn.nextLine();
+		
+		//does capitalization matter?
+		System.out.println("Should the search be case sensitive? (Y\\N)");
+		String caseSense = consoleIn.nextLine().toLowerCase();
+			
+		
+		//searching the document
+		try(Scanner fileScanner = new Scanner(fileToSearch)) {
+			int lineNum = 0;
+			while(fileScanner.hasNextLine()) {
+				lineNum++;
+				String line = fileScanner.nextLine();
+				if(caseSense.equals("y")) {
+					if (line.contains(searchPhrase)) {
+						System.out.println(lineNum + ") " + line);
+					}
+				}else {
+					if (line.toLowerCase().contains(searchPhrase.toLowerCase())) {
+						System.out.println(lineNum + ") " + line);
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Something BAD happened");
+		}
 	}
-
+	
+			
 }
+		
+		
+		
+
+	
+	
+		
+
