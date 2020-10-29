7ed80b4f6fcbb436b12475674f78f7c828af605f
Rich Seeds <rich@techelevator.com>
2020-10-29 15:43:47 -0400

    Lecture, Exercise & Tutorial - Exercise Final for 3-9 & 3-10

---
+++ module-2/09_HTTP_Web_Services_GET/exercise-final/java/src/main/java/com/techelevator/services/AuctionService.java
@@ -0,0 +1,68 @@
+package com.techelevator.services;
+
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import com.techelevator.models.Auction;
+
+public class AuctionService {
+
+    private final String BASE_URL;
+    public RestTemplate restTemplate = new RestTemplate();
+    private final ConsoleService console = new ConsoleService();
+
+    public AuctionService(String url) {
+        BASE_URL = url;
+    }
+
+
+    public Auction[] getAll() {
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(BASE_URL, Auction[].class);
+        } catch (RestClientResponseException ex) {
+            console.printError("Could not retrieve the auctions. Is the server running?");
+        } catch (ResourceAccessException ex) {
+            console.printError("A network error occurred.");
+        }
+        return auctions;
+    }
+
+    public Auction getOne(int id) {
+        Auction auction = null;
+        try {
+            auction = restTemplate.getForObject(BASE_URL + "/" + id, Auction.class);
+        } catch (RestClientResponseException ex) {
+            console.printError("Could not retrieve the auction.");
+        } catch (ResourceAccessException ex) {
+            console.printError("A network error occurred.");
+        }
+        return auction;
+    }
+
+    public Auction[] getByTitle(String title) {
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(BASE_URL + "?title_like=" + title, Auction[].class);
+        } catch (RestClientResponseException ex) {
+            console.printError("The title was not found. Please try again.");
+        } catch (ResourceAccessException ex) {
+            console.printError("A network error occurred.");
+        }
+        return auctions;
+    }
+
+    public Auction[] getByPrice(double price) {
+        Auction[] auctions = null;
+        try {
+            auctions = restTemplate.getForObject(BASE_URL + "?currentBid_lte=" + price, Auction[].class);
+        } catch (RestClientResponseException ex) {
+            console.printError("No auctions found. Please try again.");
+        } catch (ResourceAccessException ex) {
+            console.printError("A network error occurred.");
+        }
+        return auctions;
+    }
+
+}
