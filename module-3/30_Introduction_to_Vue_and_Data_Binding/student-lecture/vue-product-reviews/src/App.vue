f26264aec10e3829f0e9a4c9a35d531f0218afbc
Liam Knight <liamjknight@gmail.com>
2020-11-23 13:29:09 -0500

    vue lecture

---
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/student-lecture/vue-product-reviews/src/App.vue
@@ -0,0 +1,33 @@
+ <template>
+  <div id="app">
+    <product-reviews></product-reviews>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import ProductReviews from './components/ProductReviews.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 800px;
+  margin: 60px, auto, 0px, auto;
+}
+</style>
---
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/student-lecture/vue-product-reviews/src/components/ProductReviews.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="main">
+      <h2>Product Reviews for {{ name }}</h2>
+      <p class="description">{{ description }}</p>
+  
+    <div class="review" v-for="review in reviews" v-bind:key="review.id"><!--v-for is like a for loop in Vue, it can make this div appear for each review in the reviews array-->
+        <h4>{{ review.reviewer }}</h4>                                  <!--v-bind:key lets v-for now that the thing to iterate over is the review key in the script below-->
+        <div class="rating">
+            <img src="../assets/star.png" v-for="n in review.rating" v-bind:key="n" v-bind:title="review.rating + ' star-review'"><!--the v-bind:key here is again attached to what is ACTUALLY iterating-->
+        </div>                                                                         <!--the v-bind:title makes the img have a title when moused over that describes what is being displayed if it cannot be read otherwise.-->
+        <h3>{{ review.title }}</h3>
+        <p>{{ review.review }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'product-reviews',
+    data(){
+        return{
+            name: 'Cigar Party for Dummies',
+            description: 'This is how a party gets started!',
+            reviews: [
+                {
+                reviewer: 'Malcom Gladwell',
+                title: 'What a Book!',
+                review: 'Really is quite a book, with pages too!',
+                rating: 3,
+                favorited: false
+                },
+                {
+                reviewer: 'Liam',
+                title: 'Its aight',
+                review: 'Like real aight',
+                rating: 4,
+                favorited: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    div.main{
+        margin: 1rem;
+        padding: 15px, 10px;
+    }
+    div.review{
+        background-color: aquamarine;
+    }
+    div.rating img{
+        width: 2rem;
+    }
+</style>
